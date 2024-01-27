7fbf30115fa097c09629fae082dac33cd7791f6f
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-27 23:32:31 +0330

    Update check_first_timer.dart

--- lib/features/on_boarding/domain/usecases/check_first_timer.dart
+++ lib/features/on_boarding/domain/usecases/check_first_timer.dart
@@ -1,3 +1,11 @@
 import 'package:flutter_education_app_clean_bloc/core/usecases/usecases.dart';
+import 'package:flutter_education_app_clean_bloc/core/utils/type_defs.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/repositories/on_boarding_repo.dart';
 
-abstract class CheckFirstTimer extends UseCaseWithoutParams {}
+class CheckFirstTimer extends UseCaseWithoutParams<bool> {
+  const CheckFirstTimer(this._repo);
+  final OnboardingRepo _repo;
+
+  @override
+  ResultFuture<bool> call() async => _repo.checkFirstTimer();
+}

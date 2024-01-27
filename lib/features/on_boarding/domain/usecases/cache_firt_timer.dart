b2aed8f03f93727bdc53f3e59bea35ed5f912f62
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-27 23:17:08 +0330

    Create cache_firt_timer.dart

---
+++ lib/features/on_boarding/domain/usecases/cache_firt_timer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_education_app_clean_bloc/core/usecases/usecases.dart';
+import 'package:flutter_education_app_clean_bloc/core/utils/type_defs.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/repositories/on_boarding_repo.dart';
+
+class CacheFirstTimer extends UseCaseWithoutParams {
+  CacheFirstTimer(this._repo);
+  final OnboardingRepo _repo;
+
+  @override
+  ResultFuture<void> call() async => _repo.cacheFirstTimer();
+}

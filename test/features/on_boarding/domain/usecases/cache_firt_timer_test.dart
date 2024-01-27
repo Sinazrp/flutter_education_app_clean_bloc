bcd196e769c5fab777c67b9db5c9955b0bc6f0d2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-28 00:31:24 +0330

    Update cache_firt_timer_test.dart

--- test/features/on_boarding/domain/usecases/cache_firt_timer_test.dart
+++ test/features/on_boarding/domain/usecases/cache_firt_timer_test.dart
@@ -1,7 +1,56 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_education_app_clean_bloc/core/errors/exceptions.dart';
+import 'package:flutter_education_app_clean_bloc/core/errors/failure.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/repositories/on_boarding_repo.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/usecases/cache_firt_timer.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'on_boarding_repo.mock.dart';
 
 void main() {
-  testWidgets('cache firt timer ...', (tester) async {
-    // TODO: Implement test
+  late OnboardingRepo repo;
+  late CacheFirstTimer useCase;
+  setUp(() {
+    repo = MockOnBoarding();
+    useCase = CacheFirstTimer(repo);
+  });
+
+  test('should call the [onBoardimg.cashe] and return nll', () async {
+    //Arrange
+    when(() => repo.cacheFirstTimer())
+        .thenAnswer((_) async => const Right<Failure, void>(null));
+
+    //act
+    final result = await useCase();
+
+    //assert
+    expect(result, const Right<Failure, void>(null));
+
+    verify(() => repo.cacheFirstTimer()).called(1);
+    verifyNoMoreInteractions(repo);
+  });
+
+  test('should call the [onBoardimg.cashe] and return Failure', () async {
+    //Arrange
+    when(() => repo.cacheFirstTimer()).thenAnswer(
+      (_) async => Left<Failure, void>(
+        ServerFailure(message: 'unknown error', statusCode: 500),
+      ),
+    );
+
+    //act
+    final result = await useCase();
+
+    //assert
+    expect(
+      result,
+      Left<Failure, void>(
+        ServerFailure(message: 'unknown error', statusCode: 500),
+      ),
+    );
+
+    verify(() => repo.cacheFirstTimer()).called(1);
+    verifyNoMoreInteractions(repo);
   });
 }

2637e01ec8576f00ddc618efb91864eb9be3790e
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-28 21:26:51 +0330

    Update on_boarding_local_data_source.dart

--- lib/features/on_boarding/data/datasources/on_boarding_local_data_source.dart
+++ lib/features/on_boarding/data/datasources/on_boarding_local_data_source.dart
@@ -1,4 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 abstract class OnBoardingLocalDataSoource {
+  const OnBoardingLocalDataSoource();
   Future<void> cacheFirstTimer();
   Future<bool> checkFirstTimer();
+}
+
+const kFirstTimerKey = 'first_timer_key';
+
+class OnBoardingDataSourceImple extends OnBoardingLocalDataSoource {
+  const OnBoardingDataSourceImple(this._preferences);
+
+  final SharedPreferences _preferences;
+
+  @override
+  Future<void> cacheFirstTimer() async {
+    await _preferences.setBool(kFirstTimerKey, false);
+  }
+
+  @override
+  Future<bool> checkFirstTimer() {
+    // TODO: implement checkFirstTimer
+    throw UnimplementedError();
+  }
 }

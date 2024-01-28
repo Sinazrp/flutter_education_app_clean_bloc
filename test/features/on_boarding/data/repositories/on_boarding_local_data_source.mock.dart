cc887fcfd83b5e97805202ff64d7b53520f7b991
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-28 20:15:56 +0330

    Create on_boarding_local_data_source.mock.dart

---
+++ test/features/on_boarding/data/repositories/on_boarding_local_data_source.mock.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/data/datasources/on_boarding_local_data_source.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockOnBoardingDataSource extends Mock
+    implements OnBoardingLocalDataSoource {}

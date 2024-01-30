b1ea6a8d272d327e07041534cf91ee0b1eb6d0c8
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-30 22:00:51 +0330

    Update onboarding_cubit_state.dart

--- lib/features/on_boarding/presentation/cubit/onboarding_cubit_state.dart
+++ lib/features/on_boarding/presentation/cubit/onboarding_cubit_state.dart
@@ -2,28 +2,35 @@ part of 'onboarding_cubit_cubit.dart';
 
 sealed class OnboardingCubitState extends Equatable {
   const OnboardingCubitState();
-
   @override
   List<Object> get props => [];
 }
 
 class OnboardingCubitInitial extends OnboardingCubitState {
   const OnboardingCubitInitial();
+  @override
+  List<Object> get props => [];
 }
 
 class CasheFirstTimer extends OnboardingCubitState {
   const CasheFirstTimer();
+  @override
+  List<Object> get props => [];
 }
 
 class CheckFirstTimer extends OnboardingCubitState {
-  const CheckFirstTimer();
+  @override
+  List<Object> get props => [];
 }
 
 class UserCached extends OnboardingCubitState {
-  const UserCached();
+  @override
+  List<Object> get props => [];
 }
 
 class OnBoardingStatus extends OnboardingCubitState {
-  const OnBoardingStatus();
-  final bool isFirstTimer = 
+  const OnBoardingStatus({required this.isFirstTimer});
+  final bool isFirstTimer;
+  @override
+  List<Object> get props => [isFirstTimer];
 }

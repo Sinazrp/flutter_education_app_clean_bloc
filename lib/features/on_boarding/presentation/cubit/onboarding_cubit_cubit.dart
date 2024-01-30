c1ae5aef323d12638d6b6b3c0112c176e90f905c
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-01-30 22:19:15 +0330

    Update onboarding_cubit_cubit.dart

--- lib/features/on_boarding/presentation/cubit/onboarding_cubit_cubit.dart
+++ lib/features/on_boarding/presentation/cubit/onboarding_cubit_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/usecases/cache_firt_timer.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/usecases/check_first_timer.dart';
 
 part 'onboarding_cubit_state.dart';
 
@@ -14,4 +15,13 @@ class OnboardingCubitCubit extends Cubit<OnboardingCubitState> {
 
   final CacheFirstTimer _cacheFirstTimer;
   final CheckFirstTimer _checkFirstTimer;
+  Future<void> cacheFirstTimer() async {
+    final result = await _cacheFirstTimer();
+    result.fold((l) => OnBoardingError(l.errorMessage), (r) => UserCached());
+  }
+
+  Future<void> checkFirstTimer() async {
+    final result = await _checkFirstTimer();
+    result.fold((l) => OnBoardingError(l.errorMessage), (r) => UserCached());
+  }
 }

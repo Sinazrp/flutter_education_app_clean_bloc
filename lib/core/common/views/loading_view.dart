f66561a4e7265ed2420f8741ba99b9690c441e32
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-03 22:16:26 +0330

    Create loading_view.dart

---
+++ lib/core/common/views/loading_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_education_app_clean_bloc/core/extensions/context_extensions.dart';
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor:
+              AlwaysStoppedAnimation<Color>(context.colorTheme.secondary),
+        ),
+      ),
+    );
+  }
+}

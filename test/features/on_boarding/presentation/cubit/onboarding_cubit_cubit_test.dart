fee5ebd86a8725fddfc013bb1c9f302fb7f9126f
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-03 20:52:45 +0330

    Update onboarding_cubit_cubit_test.dart

--- test/features/on_boarding/presentation/cubit/onboarding_cubit_cubit_test.dart
+++ test/features/on_boarding/presentation/cubit/onboarding_cubit_cubit_test.dart
@@ -1,3 +1,6 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_education_app_clean_bloc/core/errors/failure.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/usecases/cache_firt_timer.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/usecases/check_first_timer.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/presentation/cubit/onboarding_cubit_cubit.dart';
@@ -11,16 +14,86 @@ class MockCheckFirstTimer extends Mock implements CheckFirstTimer {}
 void main() {
   late CacheFirstTimer cacheFirstTimer;
   late CheckFirstTimer checkFirstTimer;
-  late OnboardingCubit onboardingCubit;
+  late OnboardingCubit cubit;
   setUp(() {
     cacheFirstTimer = MockCacheFirstTimer();
     checkFirstTimer = MockCheckFirstTimer();
-    onboardingCubit = OnboardingCubit(
+    cubit = OnboardingCubit(
       cacheFirstTimer: cacheFirstTimer,
       checkFirstTimer: checkFirstTimer,
     );
   });
   test('initial state should be onboardinginitial', () {
-    expect(onboardingCubit.state, const OnboardingCubitInitial());
+    expect(cubit.state, const OnboardingCubitInitial());
+  });
+  group('cache first timer', () {
+    blocTest<OnboardingCubit, OnboardingCubitState>(
+      'should complete caching',
+      build: () {
+        when(
+          () => cacheFirstTimer(),
+        ).thenAnswer((_) async => const Right(null));
+        return cubit;
+      },
+      act: (cubit) => cubit.cacheFirstTimer(),
+      expect: () => const [CashingFirstTimer(), UserCached()],
+      verify: (_) {
+        verify(() => cacheFirstTimer()).called(1);
+      },
+    );
+    blocTest<OnboardingCubit, OnboardingCubitState>(
+      'emit cashing unsuccesfull',
+      build: () {
+        when(
+          () => cacheFirstTimer(),
+        ).thenAnswer(
+          (_) async => Left(CacheFailure(message: 'oh', statusCode: 500)),
+        );
+        return cubit;
+      },
+      act: (cubit) => cubit.cacheFirstTimer(),
+      expect: () => [const CashingFirstTimer(), const OnBoardingError('oh')],
+      verify: (_) {
+        verify(() => cacheFirstTimer()).called(1);
+      },
+    );
+  });
+
+  group('check first timer', () {
+    blocTest<OnboardingCubit, OnboardingCubitState>(
+      'should emmit checking and the status',
+      build: () {
+        when(
+          () => checkFirstTimer(),
+        ).thenAnswer((_) async => const Right(false));
+        return cubit;
+      },
+      act: (cubit) => cubit.checkFirstTimer(),
+      expect: () => [
+        const CheckingFirstTimer(),
+        const OnBoardingStatus(isFirstTimer: false)
+      ],
+      verify: (_) {
+        verify(() => checkFirstTimer()).called(1);
+      },
+    );
+    blocTest<OnboardingCubit, OnboardingCubitState>(
+      'should emmit error',
+      build: () {
+        when(
+          () => checkFirstTimer(),
+        ).thenAnswer(
+            (_) async => Left(CacheFailure(message: 'oh', statusCode: 500)));
+        return cubit;
+      },
+      act: (cubit) => cubit.checkFirstTimer(),
+      expect: () => [
+        const CheckingFirstTimer(),
+        const OnBoardingStatus(isFirstTimer: true)
+      ],
+      verify: (_) {
+        verify(() => checkFirstTimer()).called(1);
+      },
+    );
   });
 }

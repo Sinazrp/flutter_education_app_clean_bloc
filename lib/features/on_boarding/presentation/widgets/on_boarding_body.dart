290d93b5cc5fefb3e9c0e3d4b1b15a4a2edf4408
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-04 22:47:55 +0330

    Create on_boarding_body.dart

---
+++ lib/features/on_boarding/presentation/widgets/on_boarding_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/entities/page_content.dart';
+
+class OnBoardingBody extends StatelessWidget {
+  const OnBoardingBody({required this.pageContent, super.key});
+  final PageContent pageContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

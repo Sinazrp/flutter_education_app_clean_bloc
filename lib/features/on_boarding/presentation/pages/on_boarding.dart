57afdf35289425dbbf6eb23289e5b217522973ce
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-04 22:47:53 +0330

    Update on_boarding.dart

--- lib/features/on_boarding/presentation/pages/on_boarding.dart
+++ lib/features/on_boarding/presentation/pages/on_boarding.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_education_app_clean_bloc/core/common/views/loading_view.dart';
 import 'package:flutter_education_app_clean_bloc/core/common/widget/gradient_background.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/entities/page_content.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/presentation/cubit/onboarding_cubit_cubit.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/presentation/widgets/on_boarding_body.dart';
 import 'package:flutter_education_app_clean_bloc/gen/assets.gen.dart';
 
 class OnBoardingScreen extends StatefulWidget {
-  const OnBoardingScreen({super.key});
+  OnBoardingScreen({super.key});
+
   static const routeName = '/';
 
   @override
@@ -14,6 +17,7 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final pageController = PageController();
   @override
   void initState() {
     super.initState();
@@ -32,9 +36,19 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         if (state is CheckingFirstTimer || state is CashingFirstTimer) {
           return const LoadingView();
         }
+
         return GradientBackground(
           image: Assets.images.onBoardingBackground.path,
-          child: Container(),
+          child: Stack(children: [
+            PageView(
+              controller: pageController,
+              children: [
+                OnBoardingBody(pageContent: PageContent.first()),
+                OnBoardingBody(pageContent: PageContent.second()),
+                OnBoardingBody(pageContent: PageContent.third()),
+              ],
+            )
+          ]),
         );
       },
     );

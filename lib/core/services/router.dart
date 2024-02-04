0ff0a347f3b579018ef02101a3aa8e9566b1fca2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-04 22:47:50 +0330

    Update router.dart

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_education_app_clean_bloc/core/common/views/page_under_construction.dart';
+import 'package:flutter_education_app_clean_bloc/core/services/injection.dart';
+import 'package:flutter_education_app_clean_bloc/features/on_boarding/presentation/cubit/onboarding_cubit_cubit.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/presentation/pages/on_boarding.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case OnBoardingScreen.routeName:
-      return _pageBuilder((_) => const OnBoardingScreen(), settings);
+      return _pageBuilder(
+          (_) => BlocProvider(
+                create: (context) => sl<OnboardingCubit>(),
+                child: OnBoardingScreen(),
+              ),
+          settings);
     default:
       return _pageBuilder((_) => const PageUnderConstruction(), settings);
   }

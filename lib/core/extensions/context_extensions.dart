5267fcd97920454ed6528e4c557f170753bd94d3
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-07 18:03:39 +0330

    Update context_extensions.dart

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 extension ContextExt on BuildContext {
   ThemeData get theme => Theme.of(this);
   ColorScheme get colorTheme => Theme.of(this).colorScheme;
+  Size get size => MediaQuery.of(this).size;
+  double get width => size.width;
+  double get height => size.height;
 }

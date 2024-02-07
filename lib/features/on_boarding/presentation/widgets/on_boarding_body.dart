e32ccf20a3641dc6c47d65cc4863fd306eae9743
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-07 18:03:37 +0330

    Update on_boarding_body.dart

--- lib/features/on_boarding/presentation/widgets/on_boarding_body.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_education_app_clean_bloc/core/extensions/context_extensions.dart';
 import 'package:flutter_education_app_clean_bloc/features/on_boarding/domain/entities/page_content.dart';
 
 class OnBoardingBody extends StatelessWidget {
@@ -7,6 +8,13 @@ class OnBoardingBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Image.asset(
+          pageContent.image,
+          height: context.height * 0.4,
+        )
+      ],
+    );
   }
 }

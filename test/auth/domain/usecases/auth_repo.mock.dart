20080a9b8506aaf306cd2bcd08b54c3dc6069d6d
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-02-09 17:52:05 +0330

    Create auth_repo.mock.dart

---
+++ test/auth/domain/usecases/auth_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_education_app_clean_bloc/features/auth/domain/repositories/auth_repo.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepo extends Mock implements AuthRepo {}
